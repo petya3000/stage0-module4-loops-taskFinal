8c6d16bb23bc50f7249b1dfccc79e5680a9bc51c
Petya4000 <pt.bartosh54321@gmail.com>
2022-09-08 19:41:30 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int res = numberToPrint;
+        for (int i = 0; i < power - 1; i++){
+            res *= numberToPrint;
+        }
+        System.out.println(res);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 0; i < cathetusLength; i++){
+            for (int j = 0; j < cathetusLength - i - 1; j++){
+                System.out.print(" ");
+            }
+            String str = "";
+            for (int j = i + 1; j > 1; j--){
+                str += j;
+            }
+            System.out.print(str);
+            System.out.print(1);
+            System.out.println(new StringBuilder(str).reverse());
+
+
+        }
 
     }
 
